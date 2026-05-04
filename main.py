e359231ad15eec4342b7c19b2b2e8c0e510ccad1
hesam31 <132648195+hesam31@users.noreply.github.com>
2026-05-04 21:10:23 +0330

    Add files via upload

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def _patched_to_dict(self, *args, **kwargs):
 InlineKeyboardButton.__init__ = _patched_init
 InlineKeyboardButton.to_dict = _patched_to_dict
 
-BOT_TOKEN = ""
+BOT_TOKEN = "8769920545:AAHXd0IvGHnQvu1Hpb_frtR1_gB_N0ZkhfM"
 ADMIN_IDS = [81469723]
 DB_FILE = "database.json"
 CARD_NUMBER = "6037697637334522"
