f6d53dfbe335f6d1c9e247b7c9f1fbf4489402aa
hesam31 <132648195+hesam31@users.noreply.github.com>
2026-06-02 13:05:26 +0330

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,6 @@ def _patched_to_dict(self, *args, **kwargs):
 
 InlineKeyboardButton.__init__ = _patched_init
 InlineKeyboardButton.to_dict = _patched_to_dict
-
 BOT_TOKEN = "8878547383:AAHu5geWSlHo6VowuKhhSFl4tn0iFA84j1M"
 ADMIN_IDS = [81469723,1892655576]
 DB_FILE = "database.db"
